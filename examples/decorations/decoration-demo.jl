b88ec84c09626a53d5e3b485e463f0063dd9206c
Micah Halter <micah.halter@gtri.gatech.edu>
2019-11-20 08:50:43 -0500

    Added petri diagrams

--- examples/decorations/decoration-demo.jl
+++ examples/decorations/decoration-demo.jl
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# + {}
 using SemanticModels
 using SemanticModels.ModelTools.PetriModels
 using SemanticModels.ModelTools.RelOlogModels
@@ -10,6 +11,22 @@ using Catlab.Doctrines
 import TikzPictures
 import Catlab.Graphics: to_tikz
 
+import MacroTools: postwalk
+function devar(p::Petri.Model)
+    println(p.S)
+    vars = Symbol[]
+    map(p.S) do S
+        postwalk(convert(Expr, S)) do ex
+            if typeof(ex) == Symbol
+                push!(vars, ex)
+                return ex
+            end
+        end
+    end
+    return Petri.Model(vars, p.Δ)
+end
+# -
+
 # # Example
 
 # ### SIR Petri Model
@@ -20,8 +37,7 @@ import Catlab.Graphics: to_tikz
 # Create S -> I -> R model
 sir_petri = Petri.Model([S, I, R], [(S+I, 2I), (I,R)])
 
-println(sir_petri.S)
-println(sir_petri.Δ)
+Petri.Graph(devar(sir_petri))
 # -
 # ### SIR Relational Olog
 
@@ -44,8 +60,7 @@ to_tikz(reduce(⊗, generators(sir_relolog, FreeBicategoryRelations.Hom)); arrow
 # Create Susceptible to 2 diseases model
 sii_petri = Petri.Model([S, I, I′], [(S+I,  2I ), (S+I′, 2I′)])
 
-println(sii_petri.S)
-println(sii_petri.Δ)
+Petri.Graph(devar(sii_petri))
 # -
 # ### SII Relational Olog
 
@@ -68,16 +83,16 @@ f = FinSetMorph(1:3, [1, 2])
 g = FinSetMorph(1:3, [1, 2])
 # ### Decorate the Morphisms with a Petri Model
 # + {}
-# Decorate a finite set with SIR Petri Model
-dec_f_petri = Decorated(f, sir_petri)
-
 # Decorate a finite set with SII Petri Model
-dec_g_petri = Decorated(g, sii_petri)
+dec_f_petri = Decorated(f, sii_petri)
+
+# Decorate a finite set with SIR Petri Model
+dec_g_petri = Decorated(g, sir_petri)
 # -
 # ### Decorate the Same Morphisms with a Relational Olog
 
-double_f = Decorated(dec_f_petri, sir_relolog)
-double_g = Decorated(dec_g_petri, sii_relolog)
+double_f = Decorated(dec_f_petri, sii_relolog)
+double_g = Decorated(dec_g_petri, sir_relolog)
 
 # ### Create Span and Solve Pushout
 
@@ -93,7 +108,6 @@ H = CategoryTheory.pushout(S)
 H_relolog = decoration(H)
 H_petri = decoration(undecorate(H))
 
-println(H_petri.S)
-println(H_petri.Δ)
+display(Petri.Graph(devar(H_petri)))
 
 to_tikz(reduce(⊗, generators(H_relolog, FreeBicategoryRelations.Hom)); arrowtip="Stealth", labels=true)
